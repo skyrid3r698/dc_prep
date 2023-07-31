bb1646cfa24cc6c62e2bad6b727b9d8a7ac7cd17
Skyrider <79767107+skyrid3r698@users.noreply.github.com>
2023-07-31 11:41:37 +0200

    bugfix and add adconnect

--- dc_prep.ps1
+++ dc_prep.ps1
@@ -13,7 +13,8 @@ else {
     $domainname = (Get-ADDomain).DistinguishedName
     Write-Host $(Get-Date)"[INFO] Current Machine is a Domaincontroller of $domainname" -ForegroundColor Green
 }
-
+#prepare object for better downloading
+$wc = New-Object net.webclient
 
 #read configfile if available
 $configfilepath = "$($pwd.path)\dc_prep.conf"
@@ -33,6 +34,13 @@ else {
         if ($datev -eq "n") {write-host "no datev specific preperations are going to be configured.";break}
         else {$datev = Read-Host "Is this going to be a DATEV Fileserver? [y/n]"}
     }
+    $adconnect = Read-Host "Configure Azure-AD-Connect?"
+    while(1 -ne 2)
+    {
+        if ($adconnect -eq "y") {write-host "Azuer-AD-Connect is going to be configured";break} 
+        if ($adconnect -eq "n") {write-host "Azuer-AD-Connect is NOT going to be configured";break}
+        else {$adconnect = Read-Host "Configure Azure-AD-Connect? [y/n]"}
+    }
 }
 
 function create_ad_ou {
@@ -48,9 +56,13 @@ function create_ad_groups {
 
 function datev {
     New-ADGroup -Name "DATEVUSER" -SamAccountName DATEVUSER -GroupCategory Security -GroupScope Global -DisplayName "DATEVUSER" -Path "OU=Gruppen,OU=$customer_name,$domainname"
-    $wc = New-Object net.webclient
     $wc.Downloadfile("https://download.datev.de/download/datevitfix/serverprep.exe", "C:\Users\$env:USERNAME\Downloads\serverprep.exe")
 
+}
+
+function adconnect {
+    New-ADGroup -Name "M365-AD-Connect" -SamAccountName M365-AD-Connect -GroupCategory Security -GroupScope Global -DisplayName "M365-AD-Connect" -Path "OU=Gruppen,OU=$customer_name,$domainname"
+    $wc.Downloadfile("https://download.microsoft.com/download/B/0/0/B00291D0-5A83-4DE7-86F5-980BC00DE05A/AzureADConnect.msi", "C:\Users\$env:USERNAME\Downloads\AzureADConnect.exe")
 }
 
 #check if successfull
@@ -60,9 +72,9 @@ function check {
     if([adsi]::Exists("LDAP://OU=Benutzer,OU=$customer_name,$domainname")) {Write-Host $(Get-Date)"[Info] OU=Benutzer,OU=$customer_name,$domainname successfully created"} else {Write-Host $(Get-Date)"[ERROR] OU=Benutzer,OU=$customer_name,$domainname creation failed"; $exitcode +1}
     if([adsi]::Exists("LDAP://OU=Gruppen,OU=$customer_name,$domainname")) {Write-Host $(Get-Date)"[Info] OU=Gruppen,OU=$customer_name,$domainname successfully created"} else {Write-Host $(Get-Date)"[ERROR] OU=Gruppen,OU=$customer_name,$domainname creation failed"; $exitcode +1}
     if([adsi]::Exists("LDAP://OU=Terminalserver,OU=$customer_name,$domainname")) {Write-Host $(Get-Date)"[Info] OU=Terminalserver,OU=$customer_name,$domainname successfully created"} else {Write-Host $(Get-Date)"[ERROR] OU=Terminalserver,OU=$customer_name,$domainname creation failed"; $exitcode +1}
-    if ($datev -eq "y") {
-        if([adsi]::Exists("LDAP://CN=DATEVUSER,OU=Gruppen,OU=$customer_name,$domainname")) {Write-Host $(Get-Date)"[Info] OU=Terminalserver,OU=$customer_name,$domainname successfully created"} else {Write-Host $(Get-Date)"[ERROR] OU=Terminalserver,OU=$customer_name,$domainname creation failed"; $exitcode +1}
-    }
+    if ($datev -eq "y") { if([adsi]::Exists("LDAP://CN=DATEVUSER,OU=Gruppen,OU=$customer_name,$domainname")) {Write-Host $(Get-Date)"[Info] CN=DATEVUSER,OU=Gruppen,OU=$customer_name,$domainname successfully created"} else {Write-Host $(Get-Date)"[ERROR] CN=DATEVUSER,OU=Gruppen,OU=$customer_name,$domainname creation failed"; $exitcode +1}}
+    if ($adconnect -eq "y") { if([adsi]::Exists("LDAP://CN=M365-AD-Connect,OU=Gruppen,OU=$customer_name,$domainname")) {Write-Host $(Get-Date)"[Info] CN=M365-AD-Connect,OU=Gruppen,OU=$customer_name,$domainname successfully created"} else {Write-Host $(Get-Date)"[ERROR] CN=M365-AD-Connect,OU=Gruppen,OU=$customer_name,$domainname creation failed"; $exitcode +1}}
+
     if ($exitcode -eq 0) {
         Write-Host $(Get-Date)"[INFO]The Script encountered no errors."
     }
@@ -75,4 +87,5 @@ function check {
 create_ad_ou
 create_ad_groups
 if ($datev -eq "y") {datev}
+if ($adconnect -eq "y") {adconnect}
 check
